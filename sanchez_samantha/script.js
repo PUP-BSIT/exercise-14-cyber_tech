85fe456d8fbd0f0320626e608fe1bb082cfdcc8d
Samantha Nicole Sanchez <ssamanthanicole26@gmail.com>
2023-11-13 23:49:24 +0800

    add comment sorting functionality by date

--- sanchez_samantha/script.js
+++ sanchez_samantha/script.js
@@ -1,39 +1,52 @@
-let nameInput = document.getElementById("name_input");
-let commentInput = document.getElementById("comment_input");
-let submitButton = document.getElementById("submit_comment");
-let commentsList = document.querySelector(".comments-list");
+const getElement = (id) => document.getElementById(id);
+const nameInput = getElement("name_input");
+const commentInput = getElement("comment_input");
+const submitButton = getElement("submit_comment");
+const commentsList = document.querySelector(".comments-list");
 
 updateSubmitButton();
 
 nameInput.addEventListener("input", updateSubmitButton);
 commentInput.addEventListener("input", updateSubmitButton);
 submitButton.addEventListener("click", addComment);
- 
+getElement("sort_asc").addEventListener("click", () => sortComments(true));
+getElement("sort_desc").addEventListener("click", () => sortComments(false));
+getElement("comment_Form").addEventListener
+("submit", (event) => event.preventDefault());
+
 function updateSubmitButton() {
-    if (nameInput.value.trim() !== "" && commentInput.value.trim() !== "") {
-        submitButton.removeAttribute("disabled");
-    } else {
-        submitButton.setAttribute("disabled", "disabled");
-    }
+    submitButton.disabled = !(nameInput.value.trim() && 
+    commentInput.value.trim());
 }
 
 function addComment() {
-    if (nameInput.value.trim() === "" || commentInput.value.trim() === "") {
-        return; 
-    }
+    if (!nameInput.value.trim() || !commentInput.value.trim()) return;
 
-    let newComment = document.createElement("li");
-    let commenterName = document.createElement("strong");
-    commenterName.textContent = nameInput.value + ": ";
-    let commentText = document.createElement("span");
-    commentText.textContent = commentInput.value;
+    const newComment = document.createElement("li");
+    newComment.innerHTML = `<strong>${nameInput.value}:</strong> 
+    <span>${commentInput.value}</span>`;
+    newComment.dataset.timestamp = new Date().toISOString();
 
-    newComment.appendChild(commenterName);
-    newComment.appendChild(commentText);
     commentsList.appendChild(newComment);
-
     nameInput.value = "";
     commentInput.value = "";
 
-    submitButton.setAttribute("disabled", "disabled");
+    updateSubmitButton();
+}
+
+function sortComments(ascending) {
+    const allComments = Array.from(document.querySelectorAll
+        ('.comments-list > li'));
+    const sortedComments = allComments.sort((a, b) => {
+        const dateA = new Date(a.dataset.timestamp);
+        const dateB = new Date(b.dataset.timestamp);
+        return ascending ? dateA - dateB : dateB - dateA;
+    });
+
+    updateCommentsList(sortedComments);
+}
+
+function updateCommentsList(sortedComments) {
+    commentsList.innerHTML = "";
+    sortedComments.forEach((comment) => commentsList.appendChild(comment));
 }
